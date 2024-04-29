c2419b305a3e94aa2a68702ebfc57170a79d32ce
Maksym <140002546+Machokar@users.noreply.github.com>
2024-04-29 18:26:58 +0100

    Update update.js

--- helpers/update.js
+++ helpers/update.js
@@ -1,10 +1,10 @@
 import multer from "multer";
 import path from "path";
 
-const tempDir = path.join(process.cwd(), "tmp");
-
 const multerConfig = multer.diskStorage({
-  destination: tempDir,
+  destination: (req, file, cb) => {
+    cb(null, path.join("tmp"));
+  },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
   },
